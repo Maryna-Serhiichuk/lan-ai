a4550858ea6f638049e04bb3d3fc75148c1de33d
Maryna Serhiichuk <marina.serhiichuk@gmail.com>
2024-12-17 19:18:37 +0200

    add theme tsx component

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import Router from './pages';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './components/theme'
+import ThemeConfigProvider from './components/theme'
 import { AuthProvider } from 'Auth';
 
 const client = new ApolloClient({
@@ -14,9 +13,9 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <AuthProvider>
-        <ThemeProvider theme={theme}>
+        <ThemeConfigProvider>
           <Router />
-        </ThemeProvider>
+        </ThemeConfigProvider>
       </AuthProvider>
     </ApolloProvider>
   );
